3573428ecac9ae8448aac5385617d404a8b8262b
Stephen Cresswell <229672+cressie176@users.noreply.github.com>
2022-05-27 23:54:35 +0100

    Add missing code

---
+++ test/processors/empty.test.js
@@ -0,0 +1,11 @@
+const { match } = require('assert');
+const { processors: { empty } } = require('../..');
+
+describe('empty', () => {
+
+  it('should work out of the box', () => {
+    const fn = empty({ index: 2 });
+    const result = fn({ record: {} });
+    match(result.message, /^Empty message logged at .*\/test\/processors\/empty.test.js:\d+:\d+/);
+  });
+});
